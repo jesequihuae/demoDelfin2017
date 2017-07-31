4de4430cc0886ff6cc03bc1d67bed48c49f7efc3
jesequihuae <jesequihuae@gmail.com>
2017-07-31 12:32:33 -0500

    Trabajando mapa

--- DemoDelfin20172/app/src/main/java/com/example/jesus/demodelfin2017/ContP.java
+++ DemoDelfin20172/app/src/main/java/com/example/jesus/demodelfin2017/ContP.java
@@ -40,7 +40,7 @@ public class ContP extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cont_p);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarr);
         setSupportActionBar(toolbar);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
